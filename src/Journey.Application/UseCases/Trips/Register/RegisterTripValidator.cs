62586d95b1f74b79a25cf18100b548aa6a4502f8
Leo-FSardenberg <leo.f.sardenberg@gmail.com>
2024-07-15 12:01:01 -0300

    add backend

---
+++ src/Journey.Application/UseCases/Trips/Register/RegisterTripValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Journey.Communication.Requests;
+using Journey.Exception;
+
+namespace Journey.Application.UseCases.Trips.Register
+{
+    public class RegisterTripValidator : AbstractValidator<RequestRegisterTripJson>
+    {
+        public RegisterTripValidator() 
+        {
+            RuleFor(request => request.Name).NotEmpty().WithMessage(ResourceErrorMessages.NAME_EMPTY);
+            
+            RuleFor(request => request.StartDate.Date).GreaterThanOrEqualTo(DateTime.UtcNow.Date)
+                .WithMessage(ResourceErrorMessages.Invalid_Date);
+            RuleFor(request => request).Must(request => request.EndDate.Date >= request.StartDate.Date)
+                .WithMessage(ResourceErrorMessages.Invalid_Date);
+        }
+    }
+}
